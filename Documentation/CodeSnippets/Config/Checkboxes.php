d7a61cc73291a45114738fc1c156dda840156f49
lina.wolf <lwolf@w-commerce.de>
2022-05-22 09:42:35 +0200

    Automate Codesnippets

---
+++ Documentation/CodeSnippets/Config/Checkboxes.php
@@ -0,0 +1,11 @@
+<?php
+// Can be run with https://github.com/TYPO3-Documentation/t3docs-codesnippets
+// ddev exec vendor/bin/typo3  restructured_api_tools:php_domain public/fileadmin/TYPO3CMS-Reference-TCA/Documentation/CodeSnippets
+
+return [
+    [
+        "action"=> "createCodeSnippet",
+        "sourceFile"=> "typo3conf/ext/styleguide/Classes/UserFunctions/FormEngine/SelectItemSorter.php",
+        "targetFileName"=> "SelectItemSorter.rst.txt",
+    ],
+];
